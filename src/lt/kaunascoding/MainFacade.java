6cbe04e46b531c2a80ac127ca10619a5e1e543ed
Your Name <you@example.com>
2018-10-09 20:10:04 +0300

    file creation added

--- src/lt/kaunascoding/MainFacade.java
+++ src/lt/kaunascoding/MainFacade.java
@@ -6,5 +6,6 @@ public class MainFacade {
 
     public static void main(String[] args) {
         FileCRUD model = new FileCRUD();
+        model.createFile();
     }
 }
--- src/lt/kaunascoding/crud/FileCRUD.java
+++ src/lt/kaunascoding/crud/FileCRUD.java
@@ -1,7 +1,32 @@
 package lt.kaunascoding.crud;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileCRUD {
     public void createFile() {
+        System.out.println("Iveskite failo pavadinima \u263A");
+        Scanner sc = new Scanner(System.in);
+        String fileName = sc.nextLine();
+        System.out.println("Ivestas failo pavadinimas: " + fileName + " \u263B");
 
+        File file = new File(fileName);
+        if (file.exists()) {
+            System.out.println("Toks failas egzistuoja");
+        } else {
+            System.out.println("Toks failas neegzistuoja \uD83D\uDE22");
+            System.out.println("Ar norite toki faila sukurti? Y/n");
+            String arSukurti = sc.nextLine();
+            if (arSukurti.toLowerCase().equals("y")) {//sulyginame mazaja raide
+                try {
+                    file.createNewFile();
+                    System.out.println("Failas sukurtas sekmingai");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("Sum ting wong");
+                }
+            }
+        }
     }
 }
